62153dae94d49ce7a2d665e0b8abd55a37db1377
unknown <6326515@ed.tus.ac.jp>
2026-05-21 18:09:57 +0900

    Update second-period week1 title styling

--- second_period/week1/app.py
+++ second_period/week1/app.py
@@ -22,7 +22,7 @@ def add_week1_title_style(html):
 
 .period-week {
   display: inline-block;
-  font-size: 0.42em;
+  font-size: 0.52em;
   font-weight: 950;
   line-height: 1;
   margin-bottom: 0.10em;
